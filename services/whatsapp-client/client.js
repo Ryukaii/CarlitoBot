23a410fc0b041128888b3a4f11faf3e1ab438f41
Guilherme Costa <Guilhermeryukaii@gmail.com>
2024-12-07 16:22:51 -0300

    no message

--- services/whatsapp-client/client.js
+++ services/whatsapp-client/client.js
@@ -9,7 +9,15 @@ if (currentEnv === "prod") {
   // eslint-disable-next-line no-undef
   puppeteerOptions = {
     headless: true,
-    args: ["--no-sandbox"],
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-accelerated-2d-canvas",
+      "--no-first-run",
+      "--no-zygote",
+      "--disable-gpu",
+    ],
     // executablePath: '/usr/bin/google-chrome-stable'
   };
 } else if (currentEnv === "dev") {
